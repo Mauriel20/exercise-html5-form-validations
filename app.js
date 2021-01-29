a879e1cfa53880bada46abd55d8c1a5d49bfe69b
Mauriel <josueazofeifajosue@gmail.com>
2021-01-29 23:59:13 +0000

    My-work

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ window.addEventListener('load', function () {
 
     var objCaja2 = document.querySelector('#caja2');
     objCaja2.style.display = "none";
-
+  
     objFormulario.addEventListener('submit', function () {
 
-        let Card = document.querySelector('#Card');
-        let CVC = document.querySelector('#CVC');
+        let Card = document.querySelector('#Card').value;
+        let CVC = document.querySelector('#CVC').value;
         let FirstName = document.querySelector('#FirstName');
         let LastName = document.querySelector('#LastName');
         let City = document.querySelector('#City');
@@ -22,39 +22,26 @@ window.addEventListener('load', function () {
         let user1 = state.options[state.selectedIndex].text;
         console.log(user, user1);
 
+        //alertas
         if (user == 0) {
             alert("por favor selecciona un estado");
         }
 
+        if (Card == "") {
+            alert("Esta vacío")
 
-        /*function validateForm() {
-
-           /* var list = document.querySelectorAll('input[name="options"]');
-            for (var item of list) {
-                item.checked = true;
-            }
-            
-            let radios = document.querySelectorAll('input[type="radio"]');
-
-            for (var item of list) {
-                item.checked = true;
-                alert("Must check some option!"); 
-
-            }
-            
-            let formValid = false;
-
-            var i = 0;
-            while (!formValid && i < radios.length) {
-                if (radios[i].checked) formValid = true;
-                i++;
-            }
-
-            if (!formValid) { alert("Must check some option!"); }
-            return formValid;
         }
 
-        */
+        else if (/^([0-9])*$/.test(Card)) {
+            alert("El valor " + Card + " no es una letra");
+
+        }
+
+     
+       
+
+
+        // parte 2
 
         var off_payment_method = document.getElementsByName('options');
         var ischecked_method = false;
